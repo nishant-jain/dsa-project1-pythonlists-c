fbfecb47ad5d8021db2c2d5787614c90604e5bf9
nishant10058@iiitd.ac.in <nishant10058@iiitd.ac.in@9fed29d1-2f34-46a4-00f5-6c75eaf00a61>
2011-03-11 17:28:32 +0000

    updated linked list implementation. added insert function.

--- trunk/link.c
+++ trunk/link.c
@@ -7,14 +7,52 @@ struct node{
 //struct node * front;
 //struct node * rear;
 
-void addbeg()
+void display (struct node *f)
+{
+	printf("\n");
+  while(f!=NULL)
+    {
+        printf(" %d-> ",f->value);
+        f=f->next;
+    }
+	printf("End\n");
+
+}
+/*
+
+~~~~~list.append(x)
+  
+
+list.extend(L)
+  
+
+list.insert(i, x)
+  
+
+list.remove(x)
+  
+
+list.pop([i])
+  
+list.index(x)
+  
+
+~~~~~list.count(x)
+
+list.sort()
+
+
+list.reverse()
+
+*/
+void append()
 {
 
 	struct node *newnode;
 	newnode=malloc(sizeof(struct node));
 	printf("Enter value of new node ");
 	scanf("%d",&(newnode->value));
-	printf("\nnew value %d" ,newnode->value);
+	//printf("\nnew value %d" ,newnode->value);
 	newnode->next=NULL;	
 	if(front==NULL && rear==NULL)
 		{	
@@ -29,29 +67,77 @@ void addbeg()
 		}		
 }
 
-void display (struct node *f)
-{
-	
-  while(f!=NULL)
-    {
-        printf(" %d ",f->value);
-        f=f->next;
-    }
+void insert(int pos,int ele, struct node *f)
+{	int i;
+	int cur_pos=0;
+	struct node *pre;
+	pre=f;
+	struct node *newnode;
+	newnode=malloc(sizeof(struct node));
+	pre=malloc(sizeof(struct node));
+	newnode->value=ele;
+//	printf("newnode value %d",newnode->value);
+/*
+	if(f!=NULL && rear!=NULL)
+		{	if(pos==cur_pos)
+				{pre->next=newnode;
+					newnode->next=f;}	//insert a new node here
+			else {
+			cur_pos+=1;
+			pre=f;
+			f=f->next;}*/
+	if(pos==0)
+		{newnode->next=f;
+		front=newnode;
+		}
+	else{
 
+		while(cur_pos<pos)
+		{	pre=f;
+			f=f->next;
+			cur_pos++;
+		}
+	if(f!=rear)
+	{
+	//printf("previous node = %d,%d",pre->value,f->value);
+	pre->next=newnode;
+	newnode->next=f;}
+	else
+	{
+			pre->next=newnode;
+			newnode->next=f;
+	}
+	}
+	display(front);
+		
+		
+	
+}
+
+int count(struct node *f,int x)
+{	int sum=0;
+	while(f!=NULL)
+		{
+			if(f->value==x)
+				sum++;
+		}
+	return sum;
 }
 
 
 
+
+//void remove
+
 void main()
 {	front = rear=NULL;
-	addbeg();
+	append();
 	//display(front);
-	addbeg();
+	append();
 	//display(front);
-	addbeg();
+	append();append();
 	display(front);
-	
-	
-	
+	insert(4,45,front);	
+	display(front);	
 }
 
